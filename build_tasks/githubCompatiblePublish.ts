a89cbf38d94b9f1b7deba3e896dbb807c550a1e1
CtrlAltDeleteMeNot <54217898+CtrlAltDeleteMeNot@users.noreply.github.com>
2026-05-11 22:38:27 +0300

    Docs

--- build_tasks/githubCompatiblePublish.ts
+++ build_tasks/githubCompatiblePublish.ts
@@ -1,6 +1,6 @@
 import gulp from 'gulp'
 import { Configuration } from "./_configuration";
-const githubFolder = '../Prometheus_Publish'
+const githubFolder = './docs/'
 export const githubCompatiblePublish = (configuration: Configuration): gulp.TaskFunction => {
     return function githubCompatiblePublishImpl() {
         return gulp
