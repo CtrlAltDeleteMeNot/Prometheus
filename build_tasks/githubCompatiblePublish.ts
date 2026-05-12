537a97852a0d012c9a0af9db535fa9eb93c46486
CtrlAltDeleteMeNot <54217898+CtrlAltDeleteMeNot@users.noreply.github.com>
2026-05-12 14:06:03 +0300

    Automatic update

--- build_tasks/githubCompatiblePublish.ts
+++ build_tasks/githubCompatiblePublish.ts
@@ -1,10 +1,41 @@
 import gulp from 'gulp'
 import { Configuration } from "./_configuration";
+import fs from 'fs';
+import path from 'path';
+import replace from "gulp-replace";
+
+
 const githubFolder = './docs/'
+const manifest = {
+    "name": "Prometheus",
+    "short_name": "Prometheus",
+    "description": "Prometheus is the ultimate companion for crypto traders focused on USDC pairs.",
+    "icons": [
+        {
+            "src": "img/logo.svg",
+            "sizes": "any",
+            "type": "image/svg+xml"
+        }
+    ],
+    "start_url": "https://ctrlaltdeletemenot.github.io/Prometheus/",
+    "display": "standalone",
+    "background_color": "#000000",
+    "theme_color": "#13171f",
+    "orientation": "portrait",
+    "scope": "/Prometheus/",
+    "id": "/Prometheus/",
+    "lang": "en-US"
+};
 export const githubCompatiblePublish = (configuration: Configuration): gulp.TaskFunction => {
     return function githubCompatiblePublishImpl() {
+        fs.writeFileSync(
+            path.join(githubFolder, "manifest.json"),
+            JSON.stringify(manifest, null, 2)
+        );
+        const ts = Date.now(); 
         return gulp
             .src(`${configuration.appRootDir}/**/*`)
+            .pipe(replace("?v=1.0.0.version", `?v=${ts}`))
             .pipe(gulp.dest(githubFolder))
     }
 }
